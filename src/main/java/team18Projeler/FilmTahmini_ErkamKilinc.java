4cf7fd7333e11a5a514dcdffe45e293dc0a7efa4
Erkam KILINÇ <erkamkilinc55@gmail.com>
2022-11-14 19:09:42 +0300

    Completed

--- src/main/java/team18Projeler/FilmTahmini_ErkamKilinc.java
+++ src/main/java/team18Projeler/FilmTahmini_ErkamKilinc.java
@@ -1,7 +1,14 @@
 package team18Projeler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class FilmTahmini_ErkamKilinc {
-     /*
+    public static void main(String[] args) {
+
+        /*
      TASK :
       yukarıdaki film listinde bulunan filmlerden herhangi birisini kullanıcının film ismini  görmeden
       filmin index nosuna göre sectiriniz
@@ -15,8 +22,54 @@ public class FilmTahmini_ErkamKilinc {
       kullanıcının kaybedip veya kazandığını ve tahmin etmesi gereken filmin ismini  console yazdırınız
       */
 
-    public static void main(String[] args) {
 
-        System.out.println("hello");
+        List<String> film = new ArrayList<>(Arrays.asList("KELEBEK", "MUCIZE", "AYLA", "DUNKIRK",
+                "MEMENTO", "GORA", "ESKIYA", "FIGHTCLUB", "KARAKOMIK", "KILLBILL", "AMELIE", "XMEN", "OLDBOY",
+                "BATMAN", "KELOGLAN", "TOYSTORY", "MUTLULUK", "SEVEN"));
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("1'den 18'e kadar rastgele bir film numarası seçiniz...");
+        int filmNo = input.nextInt()-1;
+        String secilenFilm = film.get(filmNo);
+        int tH = tahminHakki(secilenFilm);
+
+        System.out.println("Seçtiğiniz filmin harf sayısı: "+secilenFilm.length());
+        System.out.println("Filmi tahmin etmek için "+tH+" hakkınız var.");
+        String sonuc[] = secilenFilm.replaceAll("[A-Z]","_").split("");
+
+        while(true){
+            if(tH==0){
+                System.out.println("Tahmin hakkın kalmadı. Kaybettin");
+                break;
+            }
+            System.out.println("Filmi tahmin edebilir ya da bir harf deneyebilirsiniz\n");
+            String tahmin = input.next().toUpperCase();
+            if(tahmin.equals(secilenFilm)){
+                System.out.println("Tebrikler, doğru bildiniz...");
+                break;
+            }else if(secilenFilm.contains(tahmin)){
+                tH--;
+                System.out.println("Harfi buldun...");
+
+                for(int i=0; i<secilenFilm.length(); i++){
+
+                    if(secilenFilm.charAt(i) == tahmin.charAt(0)){
+                        sonuc[i] = tahmin;
+                    }
+                }
+                System.out.println(Arrays.toString(sonuc));
+                System.out.println();
+                System.out.println("Kalan tahmin hakkın: "+tH);
+            }else if(!secilenFilm.contains(tahmin)){
+                tH--;
+                System.out.println("\nHarfi bulamadın...");
+                System.out.println(Arrays.toString(sonuc));
+                System.out.println("\nKalan tahmin hakkın: "+tH);
+            }
+        }
+
+    }
+    public static int tahminHakki(String a){
+        return a.length()*2;
     }
 }
